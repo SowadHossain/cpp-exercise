b92889a7c6f31c10f10b399c8335d6ac588550ec
Sowad <sowadhossain017@gmail.com>
2022-11-17 23:45:52 +0600

    linear scearch algorithm on array

---
+++ linearSearchAlgo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int linearSearchAlgo(int numbers[], int size, int target){
+	for(int i=0; i < size; i++){
+		if (numbers[i]==target) {
+			return i;
+			break;
+		}
+	}return -1;
+}
+
+int main(){
+	int numbers[] ={10,30 ,55, 54,545,55,7,8,6,1,11} ;
+	int target = 0;
+	int size = sizeof(numbers)/sizeof(*numbers);
+	cout << linearSearchAlgo(numbers,size,target);
+
+	return 0;
+}
