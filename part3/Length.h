c05cf86a2a7c40fee8ca8dfdf16a777b4375a731
Sowad <sowadhossain017@gmail.com>
2022-11-24 14:44:19 +0600

    overloaded comparison operator

---
+++ part3/Length.h
@@ -0,0 +1,21 @@
+#ifndef LENGTH_H
+#define LENGTH_H
+
+
+class Length{
+
+public: 
+	explicit Length(int value);
+	bool operator==(const Length& other) const;
+	bool operator==(int other) const;
+	bool operator<(const Length& other) const;
+	bool operator<=(const Length& other) const;
+	bool operator>(const Length& other) const;
+	bool operator>=(const Length& other) const;
+
+private:
+	int value;
+
+};
+
+#endif //LENGTH_H
